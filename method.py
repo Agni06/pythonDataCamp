aa1be3b38f62e1e4c740c606f36b6987b02999e9
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-03-25 20:39:44 +0530

    Create method.py

---
+++ method.py
@@ -0,0 +1,32 @@
+
+""" 
+Remove the pass statement and add a display() method to the
+Manager class that just prints the string "Manager" followed by the full name, e.g. "Manager Katie Flatcher"
+Call the .display()method from the mnginstance.
+"""
+
+class Employee:
+  MIN_SALARY = 30000    
+
+  def __init__(self, name, salary=MIN_SALARY):
+      self.name = name
+      if salary >= Employee.MIN_SALARY:
+        self.salary = salary
+      else:
+        self.salary = Employee.MIN_SALARY
+        
+  def give_raise(self, amount):
+    self.salary += amount
+
+        
+# MODIFY Manager class and add a display method
+class Manager(Employee):
+  def display(self):
+    print("Manager ", self.name)
+
+
+mng = Manager("Debbie Lashko", 86500)
+print(mng.name)
+
+# Call mng.display()
+mng.display()
